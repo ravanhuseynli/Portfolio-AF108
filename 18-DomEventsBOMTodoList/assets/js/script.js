fda157ada8cb7b4e95524dcfc1e55b60b9868873
Raven0304 <huseynlirevan19@gmail.com>
2025-04-10 00:24:26 +0400

    task 18

---
+++ 18-DomEventsBOMTodoList/assets/js/script.js
@@ -0,0 +1,82 @@
+// let numOne = document.querySelector("#num1");
+// let numTwo = document.querySelector("#num2");
+// let numThree = document.querySelector("#num3");
+// let numFour = document.querySelector("#num4");
+// let numFive = document.querySelector("#num5");
+// let numSix = document.querySelector("#num6");
+// let numSeven = document.querySelector("#num7");
+// let numEight = document.querySelector("#num8");
+// let numNine = document.querySelector("#num9");
+// let numZero = document.querySelector("#zero");
+// let numPlus = document.querySelector("#addition");
+// let numMinus = document.querySelector("#subtraction");
+// let numMultiply = document.querySelector("#multip");
+// let numDivide = document.querySelector("#division");
+// let numEqual = document.querySelector("#equals");
+// let numClear = document.querySelector("#clear");
+// let Display = document.querySelector("#display");
+// let numPoint = document.querySelector("#point");
+
+
+// numNine.addEventListener('click', () => {
+//     Display.value += numNine.textContent;
+//   });
+// numEight.addEventListener('click', () => {
+//     Display.value += numEight.textContent;
+//   });
+// numSeven.addEventListener('click', () => {
+//     Display.value += numSeven.textContent;
+//   });
+// numSix.addEventListener('click', () => {
+//     Display.value += numSix.textContent;
+//   });
+// numFive.addEventListener('click', () => {
+//     Display.value += numFive.textContent;
+//   });
+// numFour.addEventListener('click', () => {
+//     Display.value += numFour.textContent;
+//   });
+// numThree.addEventListener('click', () => {
+//     Display.value += numThree.textContent;
+//   });
+// numTwo.addEventListener('click', () => {
+//     Display.value += numTwo.textContent;
+//   });
+// numOne.addEventListener('click', () => {
+//     Display.value += numOne.textContent;
+//   });
+// numZero.addEventListener('click', () => {
+//     Display.value += numZero.textContent;
+//   });
+// numPlus.addEventListener('click', () => {
+//     Display.value += numPlus.textContent;
+//   });
+// numMinus.addEventListener('click', () => {
+//     Display.value += numMinus.textContent;
+//   });
+// numMultiply.addEventListener('click', () => {
+//     Display.value += numMultiply.textContent;
+//   });
+// numDivide.addEventListener('click', () => {
+//     Display.value += numDivide.textContent;
+//   });
+
+// numEqual.addEventListener('click', () => {
+//     if (Display.value === "") {
+//         alert("Please enter a value!");
+//     } else {
+//         Display.value = eval(Display.value);
+//     }
+// }
+// );
+// numClear.addEventListener('click', () => {
+//     Display.value = "";
+//   });
+// numPoint.addEventListener('click', () => {
+//     Display.value += numPoint.textContent;
+//   });
+
+
+// ----------------------------------------------- task 2 --------------------------------------------------------
+
+// catdirmadim :(
