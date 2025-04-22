7cc4b89e378efdbe2c20df608e98fec6a5ca2ee6
Raven0304 <huseynlirevan19@gmail.com>
2025-04-23 01:14:43 +0400

    react task 26

---
+++ 26-ReactRouterModuleCss/src/Components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../pages/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../../pages/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ 26-ReactRouterModuleCss/src/pages/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import styles from '../assets/css/Contact.module.css'
+export const Contact = () => {
+  return (
+    <div className={`${styles.formContainer} container-fluid mt-5`}>
+        <div className={`${styles.textContainer} text-center`}>
+            <i class="bi bi-chat-square-dots"></i>
+            <h2>Get in touch</h2>
+            <p>We'd love to hear from you</p>
+            </div>
+        <div className={styles['form_area']}>
+            
+            <form action="" className='form-group'>
+                <input type="text" name="" id="" className='form-control'placeholder='Full Name' />
+                <input type="text" name="" id="" className='form-control'placeholder='Full Email' />
+                <input type="text" name="" id="" className='form-control'placeholder='Full Phone Number' />
+                <textarea type="text" name="" id="" className='form-control'placeholder='Message' />
+                <button className='btn btn-primary'>Send</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
